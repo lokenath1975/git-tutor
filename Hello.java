3373d8f8caa9f67a7b45572d1ee81ea15beefb00
lokenath <loknath.mukherje@gmail.com>
2019-12-25 22:21:40 +0530

    added second function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String args){
         System.out.prinln("vs code is awesome..");
+        greeting("Hello world");
+    }
+    public static void greeting(String name){
+        System.out.println("welcome",name);
+
     }
 }
